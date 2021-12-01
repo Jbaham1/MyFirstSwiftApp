f186b434dc93b3c14c904915d2aecae4bd04fa1e
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 17:22:23 -0600

    classy Employee

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -1,5 +1,19 @@
 import UIKit
 
-var name = "Jeremiah"
-var salary = 100
-var role = "Director"
+
+class Employee{
+    
+    var name = ""
+    var salary = 0
+    var role = ""
+}
+
+let a:Int = 10
+let b:String = "Ted"
+var c:Employee = Employee()
+
+c.name = b
+c.salary = 1000
+print(c.salary)
+
+

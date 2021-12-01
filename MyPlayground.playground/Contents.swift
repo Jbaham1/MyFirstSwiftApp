9758d0ab2587f8c4bc09d3704d3b3959c0c4f35a
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 14:51:19 -0600

    switch more

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let chr = "a"
+let chr = "b"
 
 //if chr == "a"{
 //    print("the character is a")
@@ -12,7 +12,7 @@ switch chr {
 case "a":
     print("character is an a")
 case "b":
-    print("character is not an a")
+    print("character is not an a, it is a b")
 default:
     print("fallback")
 }

f8c23eb2fd549f42c3e8ef8334e10922e50baf64
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 15:44:23 -0600

    switch with a twist

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,7 @@ import UIKit
 
 var greeting = "Hello, playground"
 
-let chr = "b"
+let chr = "c"
 
 //if chr == "a"{
 //    print("the character is a")
@@ -11,8 +11,8 @@ let chr = "b"
 switch chr {
 case "a":
     print("character is an a")
-case "b":
-    print("character is not an a, it is a b")
+case "b", "c":
+    print("character is a b or c")
 default:
     print("fallback")
 }

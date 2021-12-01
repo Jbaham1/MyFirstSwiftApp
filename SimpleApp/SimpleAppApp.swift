23d3f8a03256955012f71e19b9b8f37fc61822eb
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 14:20:05 -0600

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Jeremiah Baham on 12/1/21.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

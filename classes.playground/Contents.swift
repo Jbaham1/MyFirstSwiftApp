11838fcc2dc99d9cca006a8d6be6d2b56bc10525
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 18:05:05 -0600

    updating class with func

--- classes.playground/Contents.swift
+++ classes.playground/Contents.swift
@@ -6,6 +6,12 @@ class Employee{
     var name = ""
     var salary = 0
     var role = ""
+    
+    func doWork(){
+        print("Hi my name is \(name) and I am doing work")
+        salary += 100
+    }
+    
 }
 
 let a:Int = 10
@@ -13,7 +19,16 @@ let b:String = "Ted"
 var c:Employee = Employee()
 
 c.name = b
-c.salary = 1000
+//c.salary = 1000
 print(c.salary)
+
+c.doWork()
+
+var d = Employee()
+d.name = "Sarah"
+d.role = "Manager"
+d.salary = 1000
+
+d.doWork()
 
 

2fba648117b3667219a23e9e4f607ce8f820fdca
jbaham1 <baham.jeremiah20@gmail.com>
2021-12-01 18:41:54 -0600

    bool fix

--- practiceExercises.playground/Contents.swift
+++ practiceExercises.playground/Contents.swift
@@ -1,18 +1,18 @@
 import UIKit
 //E1
-var firstName = "Jeremiah"
-var lastName = "Baham"
-let gender = "male"
-var age = 28
-var cashOnHand = 600
-
-print(firstName, lastName, gender, age, cashOnHand)
+//var firstName = "Jeremiah"
+//var lastName = "Baham"
+//let gender = "male"
+//var age = 28
+//var cashOnHand = 600
+//
+//print(firstName, lastName, gender, age, cashOnHand)
 //E2
 var firstName: String = "Jeremiah"
 var lastName: String = "Baham"
 let gender: String = "male"
 var age:Int = 28
 var cashOnHand:Double = 600
-var hasChildren:Bool = "no"
+var hasChildren:Bool = false
 
 print(firstName, lastName, gender, age, cashOnHand, hasChildren)
